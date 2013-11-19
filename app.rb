4cf21e793874b85a6001268b1425bd54d56a3aef
Guilherme Berger <guilherme.berger@gmail.com>
2013-11-19 07:56:39 -0200

    Exibe mensagem dizendo se está na PUC.

--- app.rb
+++ app.rb
@@ -3,5 +3,9 @@ require 'open-uri'
 require 'json'
 
 get '/' do
-  JSON.parse(open("http://ip-api.com/json/#{request.ip}").read)["isp"]
+  if JSON.parse(open("http://ip-api.com/json/#{request.ip}").read)["isp"] == 'Pontificia Universidade Catolica do Rio de Janeiro'
+    "Você está no Wi-Fi PUC."
+  else
+    "Você não está no Wi-Fi PUC."
+  end
 end
