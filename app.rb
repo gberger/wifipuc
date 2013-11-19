89f60aa542540c600d66ce783cfc87827c2c4b85
Guilherme Berger <guilherme.berger@gmail.com>
2013-11-19 08:11:02 -0200

    Get all (//)

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'json'
 require 'unicode'
 
-get '/' do
+get // do
   @isWifiPUC = JSON.parse(open("http://ip-api.com/json/#{request.ip}").read)["isp"] == 'Pontificia Universidade Catolica do Rio de Janeiro'
   @word = @isWifiPUC ? 'SIM' : 'NÃO'
   @connector = @isWifiPUC ? '' : 'não '
